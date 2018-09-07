ce6de7b3725d134998045cbace5566247ae33848
SunnyChopper <ishy.singh@gmail.com>
2018-09-07 17:47:22 -0500

    Landing page with video option

---
+++ database/migrations/2018_09_07_221608_create_plans_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 256);
+            $table->text('description');
+            $table->double('price');
+            $table->integer('trial');
+            $table->integer('trial_days');
+            $table->string('plan_id', 512);
+            $table->integer('impressions');
+            $table->integer('signups');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plans');
+    }
+}
