55cfa0edd0d74f651b738efd4164e17a11027c87
SunnyChopper <ishy.singh@gmail.com>
2018-09-15 00:22:29 -0500

    Another Artisan message

--- database/migrations/2018_08_27_234106_update_xml_info_table.php
+++ database/migrations/2018_08_27_234106_update_xml_info_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateXmlInfoTable2 extends Migration
+class UpdateXmlInfoTable extends Migration
 {
     /**
      * Run the migrations.
