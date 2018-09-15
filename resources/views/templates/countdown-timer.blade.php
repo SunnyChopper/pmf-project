8a08f75c5bf2af62b7ae9a02aa218540b5977e6b
SunnyChopper <ishy.singh@gmail.com>
2018-09-15 02:12:31 -0500

    Added missing CSRF field

--- resources/views/templates/countdown-timer.blade.php
+++ resources/views/templates/countdown-timer.blade.php
@@ -37,6 +37,7 @@
 				</div>
 
 				<form id="signup_form" method="post">
+					{{ csrf_field() }}
 					<div class="row">
 						<div class="col-lg-4 offset-lg-4 col-md-6 offset-md-3 col-sm-12 col-xs-12">
 							<input type="text" name="name" class="form-control" placeholder="Name" required>
