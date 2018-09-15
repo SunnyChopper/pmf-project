1b8c05e94d09a788d489fb7e353fa0b538186c0e
SunnyChopper <ishy.singh@gmail.com>
2018-09-15 00:00:53 -0500

    Artisan migrate

--- database/migrations/2018_08_27_234106_update_xml_info_table.php
+++ database/migrations/2018_08_27_234106_update_xml_info_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateXmlInfoTable extends Migration
+class UpdateXmlInfoTable2 extends Migration
 {
     /**
      * Run the migrations.
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,53 +2,75 @@
 <html lang="en">
 
 <head>
-    <!-- Required meta tags-->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <meta name="keywords" content="">
+	<!-- Required meta tags-->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="keywords" content="">
 
-    <!-- Title Page-->
-    <title>{{ $page_title }}</title>
+	<!-- Title Page-->
+	<title>{{ $page_title }}</title>
 
-    <!-- Fontfaces CSS-->
-    <link href="{{ URL::asset('css/font-face.css?v=1') }}" rel="stylesheet" media="all">
-    <link href="{{ URL::asset('vendor/font-awesome-4.7/css/font-awesome.min.css?v=1') }}" rel="stylesheet" media="all">
-    <link href="{{ URL::asset('vendor/font-awesome-5/css/fontawesome-all.min.css?v=1') }}" rel="stylesheet" media="all">
-    <link href="{{ URL::asset('vendor/mdi-font/css/material-design-iconic-font.min.css?v=1') }}" rel="stylesheet" media="all">
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-P9B5TN3');</script>
+	<!-- End Google Tag Manager -->
 
-    <!-- Bootstrap CSS-->
-    <link href="{{ URL::asset('vendor/bootstrap-4.1/bootstrap.min.css?v=1') }}" rel="stylesheet" media="all">
+	<!-- Fontfaces CSS-->
+	<link href="{{ URL::asset('css/font-face.css?v=1') }}" rel="stylesheet" media="all">
+	<link href="{{ URL::asset('vendor/font-awesome-4.7/css/font-awesome.min.css?v=1') }}" rel="stylesheet" media="all">
+	<link href="{{ URL::asset('vendor/font-awesome-5/css/fontawesome-all.min.css?v=1') }}" rel="stylesheet" media="all">
+	<link href="{{ URL::asset('vendor/mdi-font/css/material-design-iconic-font.min.css?v=1') }}" rel="stylesheet" media="all">
 
-    <!-- Vendor CSS-->
-    <link href="{{ URL::asset('vendor/animsition/animsition.min.css?v=1') }}" rel="stylesheet" media="all">
-    <link href="{{ URL::asset('vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css?v=1') }}" rel="stylesheet" media="all">
-    <link href="{{ URL::asset('vendor/wow/animate.css?v=1') }}" rel="stylesheet" media="all">
-    <link href="{{ URL::asset('vendor/css-hamburgers/hamburgers.min.css?v=1') }}" rel="stylesheet" media="all">
-    <link href="{{ URL::asset('vendor/slick/slick.css?v=1') }}" rel="stylesheet" media="all">
-    <link href="{{ URL::asset('vendor/select2/select2.min.css?v=1') }}" rel="stylesheet" media="all">
-    <link href="{{ URL::asset('vendor/perfect-scrollbar/perfect-scrollbar.css?v=1') }}" rel="stylesheet" media="all">
+	<!-- Bootstrap CSS-->
+	<link href="{{ URL::asset('vendor/bootstrap-4.1/bootstrap.min.css?v=1') }}" rel="stylesheet" media="all">
 
-    <!-- Main CSS-->
-    <link href="{{ URL::asset('css/theme.css?v=1') }}" rel="stylesheet" media="all">
-    <link href="{{ URL::asset('css/style.css?v=1') }}" rel="stylesheet" media="all">
+	<!-- Vendor CSS-->
+	<link href="{{ URL::asset('vendor/animsition/animsition.min.css?v=1') }}" rel="stylesheet" media="all">
+	<link href="{{ URL::asset('vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css?v=1') }}" rel="stylesheet" media="all">
+	<link href="{{ URL::asset('vendor/wow/animate.css?v=1') }}" rel="stylesheet" media="all">
+	<link href="{{ URL::asset('vendor/css-hamburgers/hamburgers.min.css?v=1') }}" rel="stylesheet" media="all">
+	<link href="{{ URL::asset('vendor/slick/slick.css?v=1') }}" rel="stylesheet" media="all">
+	<link href="{{ URL::asset('vendor/select2/select2.min.css?v=1') }}" rel="stylesheet" media="all">
+	<link href="{{ URL::asset('vendor/perfect-scrollbar/perfect-scrollbar.css?v=1') }}" rel="stylesheet" media="all">
+
+	<!-- Main CSS-->
+	<link href="{{ URL::asset('css/theme.css?v=1') }}" rel="stylesheet" media="all">
+	<link href="{{ URL::asset('css/style.css?v=1') }}" rel="stylesheet" media="all">
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125797404-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-125797404-1');
+	</script>
 
 </head>
 <body class="animsition">
-    @extends('layouts.js')
-    <div class="page-wrapper">
-        @extends('layouts.navbar')
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P9B5TN3"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	@extends('layouts.js')
+	<div class="page-wrapper">
+		@extends('layouts.navbar')
 
-        <div class="page-container">
-            @extends('layouts.header')
-             <div class="main-content">
-                <div class="section__content section__content--p30">
-                    @yield('content')
-                </div>
-            </div>
-        </div>
-    </div>
-    
+		<div class="page-container">
+			@extends('layouts.header')
+			 <div class="main-content">
+				<div class="section__content section__content--p30">
+					@yield('content')
+				</div>
+			</div>
+		</div>
+	</div>
+	
 </body>
 </html>
