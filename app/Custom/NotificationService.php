c8786e856d7339d98aa28b8c549124a5b113e9ee
SunnyChopper <ishy.singh@gmail.com>
2018-09-26 16:35:40 -0500

    Added skeleton class for Notifications

---
+++ app/Custom/NotificationService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Custom;
+
+class NotificationService {
+	/* Global Variables */
+
+	/* Constructor */
+	public function __construct() {
+
+	}
+
+	/* Public Functions */
+	public function notifyInactiveUsers() {
+
+	}
+
+	public function notifyOnboardingDropoffUsers() {
+
+	}
+
+	/* Private Functions */
+	private function checkForInactiveUsers() {
+		
+	}
+
+	private function mail($to, $subject, $header_text, $body) { 
+
+	}
+}
+
+?>
